8cb4194c546dc461a496e0d60640bcfc00ffc2eb
daliyajohn <daliyajohn75@gmail.com>
2017-06-05 12:50:08 +0530

    Examples for remove(),clone(),children(),parent()

--- sample.js
+++ sample.js
@@ -6,4 +6,7 @@ $(document).ready(function()
 	$('#sumbit').prop('disabled', true); // disabled button using prop() function
 	$('#sumbit').html('send'); //Change Text Inside an Element Using jQuery
 	$('#sumbit1').remove(); //Remove an Element Using jQuery
+	$('.third1').clone().appendTo('.left'); //Clone an Element Using jQuery
+	$('.first').parent().css('color', 'red'); //Target the Parent of an Element Using jQuery
+	$('.right').children().css('color', 'green'); //Target the Children of an Element Using jQuery
 });
